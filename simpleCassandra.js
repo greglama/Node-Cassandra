2cdcff68fd538b8e37d78020e029749310821200
Gregoire Albizzati <gregalbizz@gmail.com>
2018-02-14 01:35:34 +0100

    Ajout d'un certains nombre de fichiers
    pour faire un certain nombre de choses

--- simpleCassandra.js
+++ simpleCassandra.js
@@ -10,7 +10,7 @@ const Client = new cassandra.Client({
   keyspace: space
 });
 
-const query = 'INSERT INTO "companies"("id", "category_code", "crunchbase_url", "video_embeds", "deadpooled_month", "overview", "updated_at", "funding_rounds", "total_money_raised", "deadpooled_day", "email_address", "twitter_username", "milestones", "ipo", "external_links", "number_of_employees", "founded_year", "founded_month", "acquisitions", "permalink", "tag_list", "name", "created_at", "homepage_url", "products", "blog_url", "deadpooled_url", "investments", "acquisition", "deadpooled_year", "alias_list", "relationships", "partners", "founded_day", "competitions", "blog_feed_url", "providerships", "offices", "phone_number", "description", "number_of_products", "number_of_providerships") VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
+const query = 'INSERT INTO "companies"("id", "category_code", "crunchbase_url", "video_embeds", "deadpooled_month", "overview", "updated_at", "funding_rounds", "total_money_raised", "deadpooled_day", "email_address", "twitter_username", "milestones", "ipo", "external_links", "number_of_employees", "founded_year", "founded_month", "acquisitions", "permalink", "tag_list", "name", "homepage_url", "products", "blog_url", "deadpooled_url", "investments", "acquisition", "deadpooled_year", "alias_list", "relationships", "partners", "founded_day", "competitions", "blog_feed_url", "providerships", "offices", "phone_number", "description", "number_of_products", "number_of_providerships") VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
 
 
 start();
@@ -46,7 +46,6 @@ async function parse(line)
       obj.permalink,
       obj.tag_list,
       obj.name ,
-      obj.created_at,
       obj.homepage_url ,
       {"products":JSON.stringify(obj.products)} ,
       obj.blog_url ,
